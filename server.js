599c01f341ac7a4f4db9395ad8ae8e6f78e5fd71
DhineshKumar47 <dinesh5047dk@gmail.com>
2025-11-10 19:51:06 +0530

    Added .gitignore and updated MongoDB connection

--- server.js
+++ server.js
@@ -2,20 +2,23 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import ticketRoutes from "./routes/ticketRoutes.js";
+import ticketRoutes from "./routes/tickets.js";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 app.use("/api/tickets", ticketRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
